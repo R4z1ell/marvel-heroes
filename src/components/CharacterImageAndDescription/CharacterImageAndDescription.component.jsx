f8db07da48baa474f3e6cab59ec0ccd06769f531
R4z1ell <creawshaka@gmail.com>
2019-08-27 13:43:23 +0200

    small refactor

---
+++ src/components/CharacterImageAndDescription/CharacterImageAndDescription.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  CharacterImage,
+  Wrapper,
+  DescriptionContainer,
+  Description,
+  CharacterName
+} from './CharacterImageAndDescription.styles';
+
+const EMPTY_DESCRIPTION = 'Sorry, there is no description for this character.';
+
+const CharacterImageAndDescription = ({ imageUrl, name, description }) => {
+  return (
+    <Wrapper>
+      <CharacterImage src={imageUrl} alt="featured character" />
+      <DescriptionContainer>
+        <CharacterName>{name}</CharacterName>
+        <Description>
+          {description ? description : EMPTY_DESCRIPTION}
+        </Description>
+      </DescriptionContainer>
+    </Wrapper>
+  );
+};
+
+CharacterImageAndDescription.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+export default CharacterImageAndDescription;
--- src/pages/CharacterDetails/CharacterDetails.styles.js
+++ src/pages/CharacterDetails/CharacterDetails.styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { colors } from '../../global.styles';
 
 export const DetailsContainer = styled.div`
   max-width: 1288px;
@@ -9,85 +8,5 @@ export const DetailsContainer = styled.div`
 
   @media screen and (max-width: 970px) {
     margin: 80px auto 0 auto;
-  }
-`;
-
-export const Wrapper = styled.div`
-  display: flex;
-
-  @media screen and (max-width: 1360px) {
-    margin: 0 auto;
-    width: 86%;
-  }
-
-  @media screen and (max-width: 970px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-export const CharacterImage = styled.img`
-  width: 299px;
-  height: 299px;
-  border: 4px solid ${colors.mainBlack};
-
-  @media screen and (max-width: 970px) {
-    width: 200px;
-    height: 200px;
-    margin-bottom: 5%;
-  }
-`;
-
-export const DescriptionContainer = styled.div`
-  width: 100%;
-  border: 4px solid ${colors.mainBlack};
-  background-color: ${colors.plainWhite};
-  margin-left: 34px;
-  padding: 35px;
-
-  @media screen and (max-width: 1040px) {
-    width: 90%;
-  }
-
-  @media screen and (max-width: 970px) {
-    margin-left: 0;
-  }
-`;
-
-export const CharacterName = styled.h2`
-  font-size: 35px;
-  text-transform: uppercase;
-  color: ${colors.mainBlack};
-  margin-bottom: 35px;
-
-  @media screen and (max-width: 1040px) {
-    font-size: 30px;
-  }
-
-  @media screen and (max-width: 970px) {
-    font-size: 28px;
-    text-align: center;
-  }
-
-  @media screen and (max-width: 600px) {
-    font-size: 26px;
-    margin-bottom: 25px;
-  }
-`;
-
-export const Description = styled.p`
-  font-family: 'Roboto';
-  font-weight: 400;
-  font-size: 20px;
-  color: ${colors.mainBlack};
-
-  @media screen and (max-width: 1060px) {
-    font-size: 18px;
-    line-height: 22px;
-  }
-
-  @media screen and (max-width: 1000px) {
-    font-size: 17px;
   }
 `;
